11bdf0501fe016fbfe59b01d8a1b209d9694333a
Eddie Lau <eddie@touchutility.com>
2016-12-16 15:42:34 +0800

    Removed unused files.

--- AJCountryPickerDemo/Pods/Target Support Files/AJCountryPicker2/AJCountryPicker2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AJCountryPicker2 : NSObject
-@end
-@implementation PodsDummy_AJCountryPicker2
-@end
--- AJCountryPickerDemo/Pods/Target Support Files/AJCountryPicker2/AJCountryPicker2-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double AJCountryPicker2VersionNumber;
-FOUNDATION_EXPORT const unsigned char AJCountryPicker2VersionString[];
-
